ff36ff7e3f0c621b4c542dabd8b2d2e9b822c4fb
Joan Camilo Olmos Durán <55892355+joolmosd@users.noreply.github.com>
2022-02-18 22:24:53 -0500

    Update IndexController.java

--- src/main/java/com/mercadolibre/FuegoQuasar/IndexController.java
+++ src/main/java/com/mercadolibre/FuegoQuasar/IndexController.java
@@ -1,56 +1,11 @@
 package com.mercadolibre.FuegoQuasar.controller;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.mercadolibre.FuegoQuasar.model.Satelite;
-import com.mercadolibre.FuegoQuasar.model.Satelites;
-
-
 @RestController
-public class SateliteController {
-	
-
-	@PostMapping("/topsecret/")
-	public ResponseEntity<double[]>  GetLocation(@RequestBody List<Satelites> satelites){
-		
-		
-		double[] answer;
-		
-		List<Double> list = new ArrayList<>();
-		
-		for(Satelites sal : satelites) {
-			list.add(sal.getDitance());
-		}
-		
-		Satelite satelite = new Satelite();
-		
-		
-		double [] distances = new double[list.size()];
-		
-		for (int i = 0; i < distances.length; i++) {
-			distances[i] = list.get(i);
-		}
-		
-		answer = satelite.GetLocation(distances);
-		
-		
-
-		if (answer != null) {
-			
-			return ResponseEntity.status(HttpStatus.OK).body(answer);
-		} else {
-			//ERROR HTTP 404
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-
-		}
-		
-	}
-	
+public class IndexController {
+    @GetMapping("/")
+    public String index() {
+        return "Hola mundo.";
+    }
 }
