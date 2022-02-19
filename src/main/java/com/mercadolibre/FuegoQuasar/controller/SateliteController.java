62713a6c063d7794ac3c64648583010f86e7db12
Joan Camilo Olmos Durán <55892355+joolmosd@users.noreply.github.com>
2022-02-18 22:42:03 -0500

    Update SateliteController.java

--- src/main/java/com/mercadolibre/FuegoQuasar/controller/SateliteController.java
+++ src/main/java/com/mercadolibre/FuegoQuasar/controller/SateliteController.java
@@ -2,28 +2,31 @@ package com.mercadolibre.FuegoQuasar.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.mercadolibre.FuegoQuasar.model.Satelite;
 import com.mercadolibre.FuegoQuasar.model.Satelites;
 
 
 @RestController
+@RequestMapping("/topsecret/")
 public class SateliteController {
 	
 
-	@PostMapping("/topsecret/")
+	@PostMapping("")
 	public ResponseEntity<double[]>  GetLocation(@RequestBody List<Satelites> satelites){
-		
+	
+//	public Satelites  GetLocation(@RequestBody Satelites satelites){
 		
 		double[] answer;
 		
 		List<Double> list = new ArrayList<>();
+		
+		System.out.println(list.toString());
 		
 		for(Satelites sal : satelites) {
 			list.add(sal.getDitance());
@@ -46,7 +49,7 @@ public class SateliteController {
 			
 			return ResponseEntity.status(HttpStatus.OK).body(answer);
 		} else {
-			//ERROR HTTP 404
+	
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
 
 		}
